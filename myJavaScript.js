db6beb33c2e1f1ac42f9ce6f24900ab5294c4f2d
148405218+lucident@users.noreply.github.com <dsean1814@gmail.com>
2024-05-22 12:42:23 -0400

    Computer choice function was created
    I needed to create a function that randomized the computer's choice to play the game.

--- myJavaScript.js
+++ myJavaScript.js
@@ -1,11 +1,8 @@
-function myComputerChoice(params) {
-    let chicken = Math.floor(Math.random() * 3)
-    if (chicken = 2) {
-        chicken = "Rock"
-    } else if (chicken = 1){
-        chicken = "Scissors"
-    } else {
-        chicken = "Paper"
-    }
-    return chicken
+console.log("hello world")
+
+function getComputerChoice(){
+    const option = ["rock","paper","scissors"];
+    const random = Math.floor(Math.random() * 3);
+    
+    return option[random]
 }
